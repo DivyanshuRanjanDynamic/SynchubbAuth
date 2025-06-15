0c151a122f7357cb9ccb6e12a81254a49d0254fd
DivyanshuRanjanDynamic <divyanshuchannel2@gmail.com>
2025-06-16 05:13:36 +0530

    fix: Update CORS configuration to include localhost URLs for local development

--- Authentication/app.js
+++ Authentication/app.js
@@ -47,7 +47,8 @@ app.use(express.urlencoded({ extended: true }));
 
 // CORS configuration
 app.use(cors({
-    origin: [env.CLIENT_URL, 'https://www.synchubb.in'],
+    origin: [env.CLIENT_URL, 'https://www.synchubb.in','http://localhost:5173', 
+        'http://127.0.0.1:5173'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
